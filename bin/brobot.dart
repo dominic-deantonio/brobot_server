0fed4dc5b496588ecc4bdadc09a68e56180619f1
Dominic <dominicdeantonio@icloud.com>
2021-12-19 21:02:58 -0500

    Endpoint to receive commands

--- bin/brobot.dart
+++ bin/brobot.dart
@@ -1,9 +1,16 @@
 import 'package:alfred/alfred.dart';
 
+enum Dir { Stopped, Forward, Left, Right, Reverse }
+
 void main() async {
   final app = Alfred();
 
-  app.all('/test', (req, res) => 'Hello World from Dart!');
+  app.all('/go', (req, res) {
+    final int dirIndex = int.parse(req.uri.queryParameters['d'] ?? '0');
+    final Dir dir = Dir.values[dirIndex];
+    print(dir);
+    return dirIndex;
+  });
 
   await app.listen(3000);
 }
